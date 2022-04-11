30cb25aaf604d0809be1784585b69c5abe8cddaa
Katrine Christensen <katch@itu.dk>
2022-04-11 14:01:44 +0200

    Fix route stuff

--- webAPI/route.js
+++ webAPI/route.js
@@ -9,14 +9,14 @@ router.use(express.json())
 
 // route handlers
 router.post("/customers", postCustomer);
-router.get("/customers/:id", getCustomer);
+router.get("/customers/:customerId", getCustomer);
 router.get("/products/", getAllProducts);
-router.get("/products/:id", getProduct);
+router.get("/products/:productId", getProduct);
 router.get("/productCategories", getProductCategories); 
 router.get("/products/productCategories/:category", getAllProductsByCategory);
 
 router.post("/baskets", postBasketForCustomer);
 router.post("/baskets/:customerId/products", postProductInBasketForCustomer);
 router.delete("/baskets/:customerId/products/:productId", deleteProductInBasketForCustomer);
-router.get("/baskets/:customerId", getBasketForCustomer)
+router.get("/baskets/:customerId/products", getBasketForCustomer)
 
